275b5368d745634c16425678c4193f4196f229fb
Komal Gujarathi <komaltalati12@gmail.com>
2026-05-13 14:27:02 +1000

    Add global exception handling middleware

--- backend/TaskManagement.Api/Program.cs
+++ backend/TaskManagement.Api/Program.cs
@@ -1,4 +1,5 @@
 using TaskManagement.Api.Services;
+using TaskManagement.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,5 +23,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapControllers();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 app.Run();
